ca9fd36b3862df17bfba686066769b5bb171ceb2
mckellardw <dwm269@cornell.edu>
2021-05-06 16:43:43 -0400

    -README updates

--- data_versions/README.md
+++ data_versions/README.md
@@ -1,10 +1,9 @@
 # Data version info
 ### v1.1:
-- 9 additional datasets over v1.0
+- 111 total datasets, including 10c chemsitries (v2, v3, and v3.1) only
 - Aligned to mm10 using cellranger v3.1
-- Constituent datasets and metadata info an be found [here](TODO)
 
 ### v1.0:
 - Compendium shown in our [bioRxiv preprint](https://www.biorxiv.org/content/10.1101/2020.12.01.407460v2)
+- 102 total datasets, including 10x Chromium chemistries (v2 and v3) only
 - Aligned to mm10 using cellranger v3.1
-- Constituent datasets and metadata info an be found [here](TODO)
--- web_app/scripts/visListPlot.R
+++ web_app/scripts/visListPlot.R
@@ -1,8 +1,8 @@
 
-# Generate feature plots given 
+# Generate feature plots given
 visListPlot <- function(
-  seu.list, 
-  features, 
+  seu.list,
+  features,
   assay='Spatial',
   reduction="space",
   pt.size=1,
@@ -16,8 +16,8 @@ visListPlot <- function(
     }
   )
   
-  gene.lims <- c(0,max(GetAssayData(vis.seurat,assay=assay)[tmp.feat,])) 
-  
+  gene.lims <- c(0,max(GetAssayData(vis.seurat,assay=assay)[tmp.feat,]))
+
   gene.lims <- lapply(
     tmp.feat,
     FUN = function(FEAT){
@@ -28,20 +28,20 @@ visListPlot <- function(
       return(c(0,out.max))
     }
   )
-  
+
   plot.list <- list()
   for(i in 1:length(tmp.feat)){
     tmp <- lapply(
       seu.list,
-      FUN = function(SEU) 
+      FUN = function(SEU)
         FeaturePlot(
           SEU,
           slot ="data",
           features = tmp.feat[i],
           pt.size = pt.size,
           reduction=reduction
-        ) + 
-        scale_color_viridis(limits=unlist(gene.lims[i]), na.value = gray(0.42))+ #,trans="log10"  limits=c(10^-2,6), 
+        ) +
+        scale_color_viridis(limits=unlist(gene.lims[i]), na.value = gray(0.42))+ #,trans="log10"  limits=c(10^-2,6),
         # scale_color_gradientn(colors=RColorBrewer::brewer.pal(11,"Spectral")[11:1],limits=c(10^-2,1),na.value = gray(0.42)) +
         theme(
           plot.margin = unit(rep(0,4), "inches"),
@@ -55,27 +55,27 @@ visListPlot <- function(
           legend.text = element_text(size=font.size,face="bold")
         )
       # labs(color="Log-Normalized\nExpression")
-    ) 
+    )
     tmp[[1]] <- tmp[[1]] +
       theme(
         plot.title = element_text(size=font.size,face="bold.italic",  vjust=1)
-      ) + 
+      ) +
       labs(title=tmp.titles[i])
-    plot.list[[i]] <- tmp 
+    plot.list[[i]] <- tmp
   }
-  
+
   injury=c("D2", "D5", "D7")
   for(i in 1:length(plot.list[[1]]) ){
-    plot.list[[1]][[i]] <- plot.list[[1]][[i]] + 
-      labs(y=injury[i]) + 
+    plot.list[[1]][[i]] <- plot.list[[1]][[i]] +
+      labs(y=injury[i]) +
       theme(axis.title.y = element_text(size=font.size, face="bold", color="black"))
   }
-  
+
   plot.list <- lapply(
     plot.list,
-    FUN = function(X) 
+    FUN = function(X)
       wrap_plots(X, ncol=1, guides="collect")&theme(legend.position="bottom",legend.margin = margin(0,0,0,0,"inches"))
   )
-  
+
   wrap_plots(plot.list,nrow=1)
 }
