e8f29e7bf7ee66aa62ea0c29f2144a72b84b736d
mckellardw <dwm269@cornell.edu>
2021-07-20 12:27:03 -0400

    -Added CelChat R script -Added new helper functino for Visium plotting -Renamed some supplemental files to match upcoming manuscript

--- R_scripts/cellchat_github_v1.R
+++ R_scripts/cellchat_github_v1.R
@@ -41,11 +41,11 @@ scMuscle.cellchat@idents <- factor(
   scMuscle.cellchat@idents,
   levels = c(
     "B Cells","NK Cells","T Cells", "Neutrophils",
-    "Monocyte (Patrolling)","Monocyte (Inflammatory)", "Monocyte (Cxcl10+)", 
-    "M2 Macro. (Cx3cr1_lo)","M2 Macro. (Cx3cr1_hi)","Dendritic",   
+    "Monocyte (Patrolling)","Monocyte (Inflammatory)", "Monocyte (Cxcl10+)",
+    "M2 Macro. (Cx3cr1_lo)","M2 Macro. (Cx3cr1_hi)","Dendritic",
     "Endothelial (Capillary)","Endothelial (Artery)", "Endothelial (Vein)","Smooth Muscle & Pericytes",
-    "FAPs (Stem)", "FAPs (Pro-remodeling)", "FAPs (Adipogenic)","Tenocytes",   
-    "Neural",  
+    "FAPs (Stem)", "FAPs (Pro-remodeling)", "FAPs (Adipogenic)","Tenocytes",
+    "Neural",
     "Quiescent_MuSCs", "Activated_MuSCs", "Committed_Myoblasts","Fusing_Myocytes",
     "Myonuclei (Type IIx)","Myonuclei (Type IIb)"
   )
@@ -62,7 +62,7 @@ scMuscle.cellchat <- subsetData(scMuscle.cellchat) # This step is necessary even
 # CellChat processing (memory intensive for large datasets)
 scMuscle.cellchat <- identifyOverExpressedGenes(scMuscle.cellchat)
 scMuscle.cellchat <- identifyOverExpressedInteractions(scMuscle.cellchat)
-gc()
+gc() #be sure to clear up memory usage...
 
 # project gene expression data onto PPI network (optional)
 scMuscle.cellchat <- projectData(scMuscle.cellchat, PPI.mouse)
@@ -81,12 +81,44 @@ scMuscle.cellchat <- aggregateNet(scMuscle.cellchat)
 
 # Compute the network centrality scores
 scMuscle.cellchat <- netAnalysis_computeCentrality(
-  scMuscle.cellchat, 
+  scMuscle.cellchat,
   slot.name = "netP"
 ) # the slot 'netP' means the inferred intercellular communication network of signaling pathways
 
 plan("sequential") # clears memory usage from future parallelization
 gc()
+
+# Focus on FAPs -> Myogenic cells ----
+
+sources.use = c(
+  "FAPs (Stem)",
+  "FAPs (Adipogenic)",
+  "FAPs (Pro-remodeling)"
+)
+targets.use = c(
+  "Quiescent_MuSCs",
+  "Activated_MuSCs",
+  "Committed_Myoblasts",
+  "Fusing_Myocytes"
+)
+
+scMuscle.cellchat@meta$sample <- scMuscle.seurat$sample[colnames(scMuscle.cellchat@data)]
+fap.cellchat <-subsetCellChat(
+  scMuscle.cellchat,
+  idents.use=c(sources.use,targets.use)
+)
+fap.cellchat@idents <- factor(
+  fap.cellchat@idents,
+  levels= c(
+    "Quiescent_MuSCs","Activated_MuSCs","Committed_Myoblasts","Fusing_Myocytes",
+    "FAPs (Stem)",
+    "FAPs (Adipogenic)",
+    "FAPs (Pro-remodeling)"
+  )
+)
+
+# Numbers of different types of interactions between FAPs and myogenic cells
+CellChatDB.mouse$interaction$annotation[CellChatDB.mouse$interaction$pathway_name %in% fap.cellchat@netP$pathways] %>%table()
 
 # Visualization ----
 #   heatmap ----
@@ -115,7 +147,7 @@ targets.use = c(
 sub.cellchat <-subsetCellChat(
   scMuscle.cellchat,
   idents.use=c(sources.use,targets.use)
-) 
+)
 sub.cellchat@idents <- factor(
   sub.cellchat@idents,
   levels= c(
@@ -151,4 +183,280 @@ netVisual_heatmap(
   width=width,
   height=height
 )
+
+
+#     FAPs->Myo chord plot ####
+sources.use = c(
+  "Monocyte (Patrolling)",
+  "Monocyte (Inflammatory)",
+  "Monocyte (Cxcl10+)",
+  "M2 Macro. (Cx3cr1_lo)",
+  "M2 Macro. (Cx3cr1_hi)",
+  "Endothelial (Capillary)",
+  "Endothelial (Artery)",
+  "Endothelial (Vein)",
+  "Smooth Muscle & Pericytes",
+  "Tenocytes",
+  "FAPs (Stem)",
+  "FAPs (Adipogenic)",
+  "FAPs (Pro-remodeling)"
+)
+targets.use = c(
+  "Quiescent_MuSCs",
+  "Activated_MuSCs",
+  "Committed_Myoblasts",
+  "Fusing_Myocytes"
+)
+
+#plot
+par(mfrow = c(1,1), xpd=TRUE)
+netVisual_chord_gene(
+  sub.cellchat, 
+  sources.use = sources.use,
+  targets.use = targets.use,
+  slot.name = "netP",
+  thresh=10^-100,
+  small.gap=0.25,
+  big.gap=5,
+  color.use = celltype.colors,
+  show.legend = F, #legend.pos.x = 10,
+  lab.cex=0.6
+  # width=width,
+  # height=height
+)
 dev.off()
+# Focus on FAPs ---------------------------------------------------------------
+sources.use = c(
+  "Tenocytes",
+  "FAPs (Stem)",
+  "FAPs (Adipogenic)",
+  "FAPs (Pro-remodeling)"
+)
+targets.use = c(
+  "Quiescent_MuSCs",
+  "Activated_MuSCs",
+  "Committed_Myoblasts",
+  "Fusing_Myocytes"
+)
+
+# scMuscle.cellchat@meta$sample <- scMuscle.seurat$sample[colnames(scMuscle.cellchat@data)]
+fap.cellchat <-subsetCellChat(
+  scMuscle.cellchat,
+  # cells.use=Cells(scMuscle.seurat)[scMuscle.seurat$harmony_PHATE_IDs %in% c(sources.use,targets.use)]
+  idents.use=c(sources.use,targets.use)
+) 
+fap.cellchat@idents <- factor(
+  fap.cellchat@idents,
+  levels= c(
+    "Quiescent_MuSCs","Activated_MuSCs","Committed_Myoblasts","Fusing_Myocytes",
+    "Tenocytes",
+    "FAPs (Stem)",
+    "FAPs (Adipogenic)",
+    "FAPs (Pro-remodeling)"
+  )
+)
+
+#
+#       chord plots for different types of ligand-receptor interactions ----
+tmp.pairLR.use <- list()
+tmp.pairLR.use[["Cell-Cell Contact"]] <- data.frame( 
+  interaction_name=CellChatDB$interaction$interaction_name[CellChatDB$interaction$annotation=="Cell-Cell Contact"]
+)
+tmp.pairLR.use[["Secreted Signaling"]] <- data.frame( 
+  interaction_name=CellChatDB$interaction$interaction_name[CellChatDB$interaction$annotation=="Secreted Signaling"]
+)
+tmp.pairLR.use[["ECM-Receptor"]] <- data.frame( 
+  interaction_name=CellChatDB$interaction$interaction_name[CellChatDB$interaction$annotation=="ECM-Receptor"]
+)
+
+par(mfrow = c(1,3), xpd=TRUE)
+netVisual_chord_gene(
+  fap.cellchat, 
+  sources.use = sources.use,
+  targets.use = targets.use,
+  slot.name = "netP",
+  pairLR.use = tmp.pairLR.use$`Cell-Cell Contact`,
+  title.name="Cell-Cell Contact",
+  thresh=0.01,
+  small.gap=0.25,
+  big.gap=5,
+  color.use = celltype.colors,
+  show.legend = F, 
+  lab.cex=0.6
+)
+netVisual_chord_gene(
+  fap.cellchat, 
+  sources.use = sources.use,
+  targets.use = targets.use,
+  slot.name = "netP",
+  pairLR.use = tmp.pairLR.use$`Secreted Signaling`,
+  title.name="Secreted Signaling",
+  thresh=0.01,
+  small.gap=0.25,
+  big.gap=5,
+  color.use = celltype.colors,
+  show.legend = F, 
+  lab.cex=0.6
+)
+netVisual_chord_gene(
+  fap.cellchat, 
+  sources.use = sources.use,
+  targets.use = targets.use,
+  slot.name = "netP",
+  pairLR.use = tmp.pairLR.use$`ECM-Receptor`,
+  title.name="ECM-Receptor",
+  thresh=0.01,
+  small.gap=0.25,
+  big.gap=5,
+  color.use = celltype.colors,
+  show.legend = F, 
+  lab.cex=0.6
+) 
+
+#     Midkine (MK) signaling plots ####
+#       LR pair contributions bar plot ----
+pathways.show="MK"
+netAnalysis_contribution(
+  fap.cellchat, 
+  signaling = pathways.show,
+  title="Midkine Signaling" #pathways.show
+)
+
+#       Myo feature plot ----
+FeaturePlot(
+  myo.seurat,
+  cells = sample(colnames(myo.seurat)),
+  reduction='phate_harmony',
+  features=c("Ncl","Lrp1","Sdc2"),
+  combine=FALSE
+) %>% lapply(
+  FUN = function(x) x + #NoLegend() +
+    umap.theme +
+    lims(
+      x=range(myo.seurat@reductions$phate_harmony@cell.embeddings[,1]),
+      y=range(myo.seurat@reductions$phate_harmony@cell.embeddings[,2])
+    ) +
+    scale_color_viridis_c()+
+    theme(
+      plot.title=element_text(size=big.font,face="bold",hjust=0.5),
+      legend.title = element_text(size=small.font,face="bold",hjust=0.5),
+      legend.text = element_text(size=small.font),
+      legend.position = "right",
+      # legend.direction = "horizontal",
+      plot.margin = unit(rep(0,4),"inches"),
+      axis.line=element_blank(),axis.title=element_blank()
+    )+
+    labs(x="PHATE_Harmony_1",y="PHATE_Harmony_2",col="Log-Normalized\nExpression")
+) %>%
+  wrap_plots(ncol=1)
+
+#       Myogenic cell MDK receptor vln plots ----
+tmp.genes <- c("Ncl","Sdc4","Lrp1")
+
+Idents(myo.seurat)<-"bin_IDs"
+signal.vlns <- myo.seurat %>% VlnPlot(
+  features = tmp.genes,
+  idents = c(
+    "Quiescent_MuSCs",
+    "Activated_MuSCs",
+    "Committed_Myoblasts",
+    "Fusing_Myocytes"
+  ),
+  slot = "data",
+  pt.size = 0,
+  group.by="bin_IDs", 
+  # cols = bin.colors,
+  cols = celltype.colors[c(
+    "Quiescent_MuSCs",
+    "Activated_MuSCs",
+    "Committed_Myoblasts",
+    "Fusing_Myocytes")],
+  combine = F
+) %>% lapply(
+  function(x) x + 
+    vln.theme + 
+    theme(
+      plot.title = element_blank(),
+      axis.text.x = element_blank(),
+      axis.line.x=element_blank(),
+      axis.title.y=element_text(size=big.font, color="black", face="bold"),
+      axis.text.y=element_text(size=small.font, color="black"),
+      plot.margin = unit(rep(0,4),units="inches")
+    )+
+    NoLegend()+
+    scale_y_continuous(expand=c(0,0))
+)
+
+for(i in 1:length(tmp.genes)){
+  signal.vlns[[i]] <-  signal.vlns[[i]] + 
+    labs(y=tmp.genes[i]) +
+    theme(axis.title.y=element_text(face="bold.italic",size=small.font))
+}
+
+signal.vlns[[length(signal.vlns)]] <- signal.vlns[[length(signal.vlns)]] + 
+  scale_y_continuous(expand = c(0.05,0))+
+  theme(
+    axis.line.x = element_line(color="black"),
+    axis.ticks.x = element_line(color="black"),
+    axis.text.x = element_text(color="black")
+  )
+
+wrap_plots(signal.vlns, ncol=1)
+
+
+#       FAP vln plot for Mdk ----
+
+tmp.genes <- c("Mdk")
+
+lig.vlns <- fap.seurat %>% VlnPlot(
+  features = tmp.genes,
+  slot = "data",
+  pt.size = 0,
+  group.by="harmony_factorIDs", 
+  cols = celltype.colors[celltypes %in% unique(fap.seurat$harmony_factorIDs)],
+  combine = F
+) %>% lapply(
+  function(x) x + 
+    vln.theme + 
+    theme(
+      plot.title = element_blank(),
+      axis.text.x = element_blank(),
+      axis.line.x=element_blank(),
+      axis.title.y=element_text(size=big.font, color="black", face="bold"),
+      axis.text.y=element_text(size=small.font, color="black"),
+      plot.margin = unit(rep(0,4),units="inches")
+    )+
+    NoLegend()+
+    scale_y_continuous(expand=c(0,0))
+)
+
+for(i in 1:length(tmp.genes)){
+  lig.vlns[[i]] <-  lig.vlns[[i]] + 
+    labs(y=tmp.genes[i]) +
+    theme(axis.title.y=element_text(face="bold.italic",size=small.font))
+}
+
+lig.vlns[[length(lig.vlns)]] <- lig.vlns[[length(lig.vlns)]] + 
+  scale_y_continuous(expand = c(0.05,0))+
+  theme(
+    axis.line.x = element_line(color="black"),
+    axis.ticks.x = element_line(color="black"),
+    axis.text.x = element_text(color="black")
+  )
+
+wrap_plots(
+  wrap_plots(signal.vlns, ncol=1)&theme(axis.text.x=element_blank()),
+  plot_spacer(),
+  wrap_plots(lig.vlns,ncol=1)&theme(axis.text.x=element_blank()), 
+  ncol=1, heights = c(3,0.1,1)
+)
+
+#
+#       Visium feature plots ----
+# See 'helper_functions_v1.R' for this function
+visListPlot(
+  seu.list = seu.list,
+  features = c("Mdk","Ncl","Sdc4","Lrp1"),
+  pt.size = 0.2*2,
+  font.size = small.font*2
+)
--- R_scripts/helper_functions_v1.R
+++ R_scripts/helper_functions_v1.R
@@ -110,6 +110,114 @@ get_cell_type_model <-
     return(ref.mat)
   }
 
+# Generate feature plots given a list of Visium/Seurat objects
+visListPlot <- function(
+  seu.list,
+  features=NULL,
+  alt.titles=NULL, # alternative titles for genes/features being passed
+  sample.titles=NULL, # Sample IDs (y-axis labels)
+  assay='Spatial',
+  reduction="space",
+  slot="data",
+  legend.position="bottom",
+  pt.size=1,
+  font.size=8,
+  verbse=FALSE
+){
+  require(Seurat)
+  require(ggplot2)
+  require(viridis)
+  
+  if(verbose){cat("Plotting Visium data!\n")}
+  
+  if(is.null(alt.titles)){
+    alt.titles=features
+  }
+  
+  seu.list <- lapply(
+    seu.list,
+    FUN = function(SEU){
+      SEU@active.assay <- assay
+      return(SEU)
+    }
+  )
+  
+  # Get expression limits for each gene, across all datasets
+  gene.lims <- lapply(
+    features,
+    FUN = function(FEAT){
+      out.max <- lapply(
+        seu.list,
+        FUN = function(SEU){
+          if(FEAT %in% rownames(SEU)){
+            return(max(GetAssayData(SEU,assay=assay)[FEAT,]))
+          }else if(FEAT %in% colnames(SEU@meta.data)){
+            return(max(SEU@meta.data[,FEAT]))
+          }else{
+            message(FEAT, " not found!")
+          }
+        }
+      ) %>% unlist() %>% max()
+      return(c(0,out.max))
+    }
+  )
+  
+  plot.list <- list()
+  for(i in 1:length(features)){
+    tmp <- lapply(
+      seu.list,
+      FUN = function(SEU)
+        FeaturePlot(
+          SEU,
+          slot = slot,
+          features = features[i],
+          pt.size = pt.size,
+          reduction=reduction
+        ) +
+        scale_color_viridis(limits=unlist(gene.lims[i]), na.value = gray(0.42))+
+        theme(
+          plot.margin = unit(rep(0,4), "inches"),
+          axis.ticks = element_blank(),
+          axis.text=element_blank(),
+          axis.title = element_blank(),
+          axis.line=element_blank(),
+          plot.title = element_blank(),
+          legend.position=legend.position,
+          legend.title = element_text(size=font.size,face="bold", hjust=0.5),
+          legend.text = element_text(size=font.size,face="bold")
+        )
+    )
+    tmp[[1]] <- tmp[[1]] +
+      theme(
+        plot.title = element_text(size=font.size,face="bold.italic",  vjust=1)
+      ) +
+      labs(title=alt.titles[i])
+    plot.list[[i]] <- tmp
+  }
+  
+  
+  for(i in 1:length(plot.list[[1]]) ){
+    plot.list[[1]][[i]] <- plot.list[[1]][[i]] +
+      theme(axis.title.y = element_text(size=font.size, face="bold", color="black"))
+    
+    if(!is.null(sample.titles)){ #add sample titles
+      plot.list[[1]][[i]] <- plot.list[[1]][[i]] +
+        labs(y=sample.titles[i])
+    }
+  }
+  
+  plot.list <- lapply(
+    plot.list,
+    FUN = function(X)
+      wrap_plots(X, ncol=1, guides="collect")&theme(legend.position=legend.position, legend.margin = margin(0,0,0,0,"inches"))
+  )
+  
+  if(verbose){cat("Done plotting Visium data!\n")}
+  
+  return(
+    wrap_plots(plot.list,nrow=1)
+  )
+}
 
 # DoubletFinder helper functions ####
 
